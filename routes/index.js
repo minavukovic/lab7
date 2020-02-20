851480e9d8023908c5c8a05c8a262fe8ee9868d4
minavukovic <mivukovi@ucsd.edu>
2020-02-19 17:46:37 -0800

    lab7

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects["viewAlt"] = false;
+	// console.log(projects);
+  	response.render('index', projects);
+};
+
+exports.viewAlt = function(request, response) {
+	projects["viewAlt"] = true;
+	// console.log("alt " + projects);
   	response.render('index', projects);
 };
